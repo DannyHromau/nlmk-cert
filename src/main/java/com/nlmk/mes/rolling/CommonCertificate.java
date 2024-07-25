03f60da9153b880ffa6051b8623d82a859e710bd
Hromau <dgromov770@gmail.com>
2024-07-25 08:09:00 +0300

    add common certificate

---
+++ src/main/java/com/nlmk/mes/rolling/CommonCertificate.java
@@ -0,0 +1,11 @@
+package com.nlmk.mes.rolling;
+
+public class CommonCertificate {
+    private String ts;
+    private Operation op;
+    private PkType pk;
+    private SystemMetadata sys;
+    private KafkaMetadata metadata;
+    private Data data;
+
+}
